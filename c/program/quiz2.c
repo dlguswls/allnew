1cc7783c4f7b18596797e8ca24ef2a3db2502f7b
Hyunjin Lee <dlskadn77@naver.com>
2023-03-20 14:11:00 +0900

    primenumber

--- c/program/quiz2.c
+++ c/program/quiz2.c
@@ -3,17 +3,10 @@
 
 void main() {
   int n;
-  printf("Input Number : ");
-  scanf("%d", &n);
-  int nn;
-  for (i=2;i<=n;i++){
-    if (n % i = 0){
-      nn = i
-      break;
-    }
-  }
-  if(checkeod(n) == 0)
-    printf("%d is even number~!! \n", n);
+  printf("\ninput integer => ");
+  scanf("%d", &n)
+  if (checkprime(n) == n)
+    printf("%d is prime number~!! \n", n);
   else
-    printf("%d is odd number~!! \n", n);
+    printf("%d is not prime number~!! \n", n);
 }
